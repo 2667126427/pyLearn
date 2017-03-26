08695c4247ee1c8f7e677774acd78421fd2a6660
Yaning Wang <2667126427@qq.com>
2017-03-26 20:02:16 +0800

    get average of several numbers you input.

--- Souce/average.py
+++ Souce/average.py
@@ -1,8 +1,8 @@
 try:
     count = 0.0
-    sum = 0.0
+    sum = 0
     while True:
-        line = input("enter a number or Enter to finish")
+        line = input("enter a number or Enter to finish:")
         if line is not "":
             sum += int(line)
             count += 1
