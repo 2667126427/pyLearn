cc21eeae409f9d97f07107e005c83cbd8b45e11e
huster <2667126427@qq.com>
2017-03-28 19:08:05 +0800

    Rename the folder "Souce" to "Source"

---
+++ Source/awful_poetry.py
@@ -0,0 +1,13 @@
+import random
+
+first = ["the ", "a ", "another "]
+obj = ["cat ", "dog ", "man ", "woman "]
+verb = ["sang ", "ran ", "jumped "]
+adv = ["loudly ", "quietly ", "well ", "badly "]
+
+i = 5
+while i > 0:
+    print(random.choice(first) + random.choice(obj) + random.choice(verb) + random.choice(adv))
+    i -= 1
+我们=29
+print(我们)
