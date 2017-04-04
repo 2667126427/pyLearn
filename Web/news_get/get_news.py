ca2edfbaedd116e4212cfc06d03e8d0ebf97c159
huster <2667126427@qq.com>
2017-04-04 12:27:25 +0800

    That's all right, yeah.

--- Web/news_get/get_news.py
+++ Web/news_get/get_news.py
@@ -1,6 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
+illegals = ['\\', '/', ':', '*', '?', '"', '<', '>', '|']
+
+
+def get_links(url):
+    res = requests.get(url)
+    res.encoding = 'utf-8'
+    soup = BeautifulSoup(res.text, 'html.parser')
+    _news_links = []
+    for item in soup.select('.news-item'):
+        if len(item.select('h2')) > 0:
+            _news_links.append(item.select('a')[0]['href'])
+    return _news_links
+
 
 def get_details(url):
     detail = requests.get(url)
@@ -12,40 +25,20 @@ def get_details(url):
     for para in body.select('p'):
         _text.append(para.text.strip())
     _text = '\r\n    '.join(_text)
+    for illegal in illegals:
+        _title = _title.replace(illegal, ' ')
+    _text = _text.replace(u'\xa0', u'').replace(u'\u2022', '*')
     return [_title, _text]
 
 
-res = requests.get('http://news.sina.com.cn/china/')
-res.encoding = 'utf-8'
-soup = BeautifulSoup(res.text, 'html.parser')
-news_links = []
-for item in soup.select('.news-item'):
-    if len(item.select('h2')) > 0:
-        news_links.append(item.select('a')[0]['href'])
-
-'''
-title, text = get_details(news_links[0])
-title = str(title).replace('|', ' ')
-file_name = title + '.txt'
-print(title)
-print(text)
-fw = open(file_name, 'w')
-text.replace(u"\xa0", " ")
-fw.write(text)
-fw.close()
-'''
 i = 0
+news_links = get_links('http://news.sina.com.cn/china/')
 for news in news_links:
-    try:
-        title, text = get_details(news)
-        title = str(title).replace('|', ' ')
-        file_name = str(i + 1).zfill(2) + ' ' + title + '.txt'
-        fw = open(file_name, 'w')
-        text.replace(u"\xa0", " ")
-        fw.write(text)
-        fw.close()
-        i += 1
-        print(file_name + ' was finished!')
-    except UnicodeEncodeError as err:
-        print(err)
+    title, text = get_details(news)
+    file_name = str(i + 1).zfill(2) + ' ' + title + '.txt'
+    fw = open(file_name, 'w')
+    fw.write(text)
+    fw.close()
+    i += 1
+    print(file_name + ' was finished!')
 print('All was finished. ' + str(i) + ' in total')
