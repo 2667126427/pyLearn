a7142a79cadcb1e33b7d37931725966825fd581a
huster <2667126427@qq.com>
2017-04-04 11:39:16 +0800

    Sina news.

--- Web/news_get/get_news.py
+++ Web/news_get/get_news.py
@@ -1,31 +1,51 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
 
-def get_url(page_url):
-    news_url_list = []
-    res = requests.get(page_url)
-    res.encoding = 'utf-8'
-    soup = BeautifulSoup(res.text, 'html.parser')
-    for news in soup.select('.news-item'):
-        if len(news.select('h2')) > 0:
-            news_url_list.append(news.select('a')[0]['href'])
-    return news_url_list
+def get_details(url):
+    detail = requests.get(url)
+    detail.encoding = 'utf-8'
+    _soup = BeautifulSoup(detail.text, 'html.parser')
+    _title = _soup.select('title')[0].text
+    body = _soup.select('#artibody')[0]
+    _text = []
+    for para in body.select('p'):
+        _text.append(para.text.strip())
+    _text = '\r\n    '.join(_text)
+    return [_title, _text]
 
 
-def get_details(news_url):
-    res = requests.get(news_url)
-    res.encoding = 'utf-8'
-    soup = BeautifulSoup(res.text, 'html.parser')
-    if len(soup.select('#artibodyTitle')) > 0:
-        title = soup.select('#artibodyTitle')[0].text.strip()
-        content = soup.select('#artibody')[0].text.strip()
-        time = soup.select('.time-source')[0].text.strip()
-        return {time, title, content}
-    else:
-        return None
+res = requests.get('http://news.sina.com.cn/china/')
+res.encoding = 'utf-8'
+soup = BeautifulSoup(res.text, 'html.parser')
+news_links = []
+for item in soup.select('.news-item'):
+    if len(item.select('h2')) > 0:
+        news_links.append(item.select('a')[0]['href'])
 
-url = 'http://news.sina.com.cn/china/'
-news_urls = get_url(url)
-for news in news_urls:
-    print(get_details(news))
+'''
+title, text = get_details(news_links[0])
+title = str(title).replace('|', ' ')
+file_name = title + '.txt'
+print(title)
+print(text)
+fw = open(file_name, 'w')
+text.replace(u"\xa0", " ")
+fw.write(text)
+fw.close()
+'''
+i = 0
+for news in news_links:
+    try:
+        title, text = get_details(news)
+        title = str(title).replace('|', ' ')
+        file_name = str(i + 1).zfill(2) + ' ' + title + '.txt'
+        fw = open(file_name, 'w')
+        text.replace(u"\xa0", " ")
+        fw.write(text)
+        fw.close()
+        i += 1
+        print(file_name + ' was finished!')
+    except UnicodeEncodeError as err:
+        print(err)
+print('All was finished. ' + str(i) + ' in total')
