811637f34ef18d48c172501d916d5ce443cd7fea
hustr <wangyaning6166@gmail.com>
2017-08-11 19:39:25 +0800

    update the code

--- Web/bing.py
+++ Web/bing.py
@@ -2,31 +2,36 @@ import os
 import requests
 
 
-# 想不到居然这样找到了链接
+#get the picture link
 def get_picture_link(url):
     res = requests.get(url)
     text = res.text
-    # 靠字符串搜索
+    # just sraech the header
     start_str = "g_img={url: \"/"
     start = text.find(start_str)
+    # search the end
     end = text.find("\",id", start)
     result = url + text[start + len(start_str) - 1:end]
     print("Find the url: " + result)
+    # return the result
     return result
 
 
+# save the picture
 def save_picture(link):
     res = requests.get(link)
+    # transform to a string
     link = str(link)
+    # find the start of name
     last = link.rfind("/")
-    # 让名字好看一点
+    # get the end index
     _index = link.find("_")
     f_name = link[last + 1: _index] + ".jpg"
-    # 判断文件是否存在
+    # check if the file has existed
     if os.path.exists(f_name):
         print(f_name + " has existed")
     else:
-        # 不存在就保存一下吧
+        # save the file
         fout = open(f_name, "wb")
         fout.write(res.content)
         fout.close()
@@ -34,7 +39,7 @@ def save_picture(link):
 
 
 if __name__ == '__main__':
-    # bing的首页地址
+    # the homepage of bing
     url = "http://www.bing.com"
     link = get_picture_link(url)
     save_picture(link)
