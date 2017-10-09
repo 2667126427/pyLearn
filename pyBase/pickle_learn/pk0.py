8145cd64480029167cd56904dbdec5b4803b2fa4
hustr <wangyaning6166@gmail.com>
2017-10-09 22:36:52 +0800

    pickle learn

---
+++ pyBase/pickle_learn/pk0.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import pickle
+
+df = pd.DataFrame([['Wang', 29], ['Li', 28]])
+df.columns = ['Name', 'Age']
+
+# 哈哈哈，我真没事找事
+with open('out.pickle', 'wb') as pickle_out:
+    pickle.dump(df, pickle_out)
+
+'''
+read operation
+data = pd.read_pickle('out.pickle')
+print(data.head())
+'''
